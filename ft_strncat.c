c512eaa622e00e7ada43b8b187cb4e5da45c3f10
Wisp Zei <wzei@e3r2p5.21-school.ru>
2018-12-02 01:32:56 +0300

    string cat

--- ft_strncat.c
+++ ft_strncat.c
@@ -1,19 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wzei <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/30 23:10:20 by wzei              #+#    #+#             */
-/*   Updated: 2018/12/02 00:50:43 by wzei             ###   ########.fr       */
+/*   Created: 2018/12/02 00:51:18 by wzei              #+#    #+#             */
+/*   Updated: 2018/12/02 01:32:04 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	ft_strcat(char *s1, const char *s2)
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	ft_strcpy(s1 + ft_strlen(s1), s2);
+	unsigned int	len0;
+	unsigned int	len1;
+
+	len0 = ft_strlen(s1);
+	len1 = ft_strlen(s2);
+	if (len1 < n)
+		ft_strcpy(&s1[len0], s2);
+	else
+	{
+		strncpy(&s1[len0], s2, n);
+		s1[len0 + n] = '\0';
+	}
 	return (s1);
 }
