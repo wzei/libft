9564320c336ad62f8a4ba69f21628f93c1b86923
Wisp Zei <wzei@e3r2p5.21-school.ru>
2018-12-06 22:54:51 +0300

    another git commit

---
+++ ft_memalloc.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/04 19:47:03 by wzei              #+#    #+#             */
+/*   Updated: 2018/12/06 01:58:07 by wzei             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void			*ret;
+	size_t			iter;
+	unsigned char	*uc;
+
+	ret = malloc(size);
+	if (ret != NULL)
+	{
+		uc = (unsigned char *)ret;
+		iter = 0;
+		while (iter < size)
+		{
+			uc[iter] = 0;
+		}
+	}
+	return (ret);
+}
