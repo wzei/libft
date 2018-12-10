48f9573e68ba3f4dd684a42268bfb01fbc88e7e4
Wisp Zei <wzei@e3r2p5.21-school.ru>
2018-12-10 21:10:06 +0300

    debug ft_memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,24 +6,26 @@
 /*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 01:57:41 by wzei              #+#    #+#             */
-/*   Updated: 2018/12/06 21:15:15 by wzei             ###   ########.fr       */
+/*   Updated: 2018/12/10 20:51:30 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
+	unsigned char		ch;
 	unsigned char		*tp;
 	const unsigned char	*fp;
 
-	tp = dst;
-	fp = src;
-	if (n)
+	ch = (unsigned char)c;
+	tp = (unsigned char *)dst;
+	fp = (const unsigned char *)src;
+	if (n != 0)
 	{
-		while (--n != 0)
+		while (n-- != 0)
 		{
-			if ((*tp++ = *fp++) == c)
+			if ((*tp++ = *fp++) == ch)
 				return (dst);
 		}
 	}
