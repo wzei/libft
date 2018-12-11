f17d6ef5ba7c86bdf4ca69b062ea5aca22c25f79
Wisp Zei <wzei@e3r2p5.21-school.ru>
2018-12-11 06:08:09 +0300

    bzero implementation

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,11 @@
 /*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 01:21:06 by wzei              #+#    #+#             */
+<<<<<<< HEAD
 /*   Updated: 2018/12/18 02:34:16 by wzei             ###   ########.fr       */
+=======
+/*   Updated: 2018/12/11 06:06:58 by wzei             ###   ########.fr       */
+>>>>>>> bzero implementation
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +18,20 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	unsigned char	*uptr_s;
-	size_t			iter;
+	unsigned long int	u_mask;
+	unsigned long int	*ulp_s;
+	unsigned char		*ucp_s;
+	unsigned char		u_z;
+	size_t				iter;
 
-	iter = 0;
-	uptr_s = (unsigned char*)s;
-	while (iter < n)
-	{
-		uptr_s[iter] = 0;
-		iter++;
-	}
+	iter = n / sizeof(long int);
+	ulp_s = (unsigned long int *)s;
+	ucp_s = (unsigned char *)s;
+	u_mask = 0;
+	u_z = 0;
+	while (iter--)
+		*ulp_s++ = u_mask;
+	iter = n % sizeof(long int);
+	while (iter--)
+		ucp_s[n - iter - 1] = u_z;
 }
