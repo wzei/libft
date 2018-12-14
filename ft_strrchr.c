f4600812bf931d3b505816e23737c21dc4b6b1e9
Wisp Zei <wzei@e3r2p5.21-school.ru>
2018-12-14 06:52:55 +0300

    ft_strrchr correction

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,13 +6,13 @@
 /*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/02 03:04:38 by wzei              #+#    #+#             */
-/*   Updated: 2018/12/07 11:50:01 by wzei             ###   ########.fr       */
+/*   Updated: 2018/12/10 21:11:07 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchar(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
 	char *save;
 	char chr;
