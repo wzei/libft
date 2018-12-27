70e51e427066b22d9e95ec28278756d29a80c443
Wisp Zei <wzei@e3r2p5.21-school.ru>
2018-12-27 18:45:58 +0300

    additional NULL protection added

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wzei <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 01:40:40 by wzei              #+#    #+#             */
-/*   Updated: 2018/11/29 20:56:27 by wzei             ###   ########.fr       */
+/*   Updated: 2018/12/27 17:41:31 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,10 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	uptr_dst = (unsigned char*)dst;
 	uptr_src = (unsigned char*)src;
 	iter = 0;
+	if (!dst && !src)
+	{
+		return (NULL);
+	}
 	while (iter < n)
 	{
 		uptr_dst[iter] = uptr_src[iter];
