133249bd06b560062274c2ea2837d2350457e462
Wisp Zei <wzei@e3r2p5.21-school.ru>
2019-01-05 09:12:35 +0300

    Implementation aligned according to C99 standard.

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/16 08:38:54 by wzei              #+#    #+#             */
-/*   Updated: 2018/12/18 02:32:28 by wzei             ###   ########.fr       */
+/*   Updated: 2019/01/04 20:20:39 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,5 @@
 
 int	ft_atoi(const char *str)
 {
-	int i;
-	int nbr;
-	int negative;
-
-	nbr = 0;
-	negative = 0;
-	i = 0;
-	while (ft_iswhtspc(str[i]))
-		i++;
-	if (str[i] == '-')
-		negative = 1;
-	if (str[i] == '+' || str[i] == '-')
-		i++;
-	while (ft_isdigit(str[i]))
-	{
-		nbr *= 10;
-		nbr += (int)str[i] - '0';
-		i++;
-	}
-	if (negative == 1)
-		return (-nbr);
-	else
-		return (nbr);
+	return ((int)ft_strtol(str, (char **)NULL, 10));
 }
