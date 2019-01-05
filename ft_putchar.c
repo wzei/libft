f4512f7c346ac0011c8f92467c664684bfd0a4ec
Wisp Zei <wzei@e3r2p5.21-school.ru>
2019-01-05 09:13:36 +0300

    Failed Unicode support removed.

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/14 13:10:34 by wzei              #+#    #+#             */
-/*   Updated: 2018/12/18 02:32:28 by wzei             ###   ########.fr       */
+/*   Updated: 2019/01/04 19:22:00 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,7 @@
 void	ft_putchar(char c)
 {
 	unsigned char			ch;
-	static unsigned char	buf[2];
 
 	ch = (unsigned char)c;
-	if ((ch & 0x80) != 0)
-	{
-		if (((ch & 0xC0) != 0) && ((~ch) & 0x20) != 0)
-		{
-			buf[0] = ch;
-			return ;
-		}
-		if (((~ch) & 0x70) != 0)
-		{
-			buf[1] = ch;
-			write(1, &buf, 2);
-			return ;
-		}
-	}
 	write(1, &ch, 1);
 }
