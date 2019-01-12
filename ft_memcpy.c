526a9d8557c4ede4fe69d5c37ec40a9bfe4ea4d5
Wisp Zei <wzei@e3r2p5.21-school.ru>
2019-01-12 21:16:50 +0300

    ft_memcpy merge with master

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,11 +6,7 @@
 /*   By: wzei <wzei@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 01:40:40 by wzei              #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2018/12/27 17:41:31 by wzei             ###   ########.fr       */
-=======
-/*   Updated: 2018/12/11 08:19:59 by wzei             ###   ########.fr       */
->>>>>>> fast ft_memcpy implementation
+/*   Updated: 2019/01/12 21:16:05 by wzei             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +20,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	unsigned char		*upc_src;
 	size_t				iter;
 
-<<<<<<< HEAD
-	uptr_dst = (unsigned char*)dst;
-	uptr_src = (unsigned char*)src;
-	iter = 0;
-	if (!dst && !src)
-	{
-		return (NULL);
-	}
-	while (iter < n)
-	{
-		uptr_dst[iter] = uptr_src[iter];
-		iter++;
-	}
-=======
 	upl_dst = (unsigned long int*)dst;
 	upl_src = (unsigned long int*)src;
 	upc_dst = (unsigned char*)dst;
@@ -48,6 +30,5 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	iter = n % sizeof(unsigned long int);
 	while (iter--)
 		upc_dst[n - iter - 1] = upc_src[n - iter - 1];
->>>>>>> fast ft_memcpy implementation
 	return (dst);
 }
